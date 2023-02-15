95ace59f3b4d8ddc8bb0248d22c2599f675a2fde
Adyasha Samal <adyasamal2001@gmail.com>
2023-02-15 19:42:35 +0530

    minor changes

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,7 +5,7 @@ const footer = () => {
     <div style={{display:'flex', justifyContent:'center',marginTop:30, backgroundColor:"black", color:"white",padding:8,
     left: 0,
     bottom: 0,
-    right: 0, width:"100%"}}><div>© Team-Psych 2022 All Rights Reserved</div></div>
+    right: 0, width:"100%"}}><div>© adyasha All Rights Reserved</div></div>
     )
 }
 
